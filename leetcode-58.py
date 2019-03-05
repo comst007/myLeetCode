a7eb7191b45c1fcbe91bd667bc9fc7e8283fa86b
Comst <liuzhao88108@gmail.com>
2019-03-06 01:49:45 +0800

    leetcode 58

---
+++ leetcode-58.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
+        if not n:
+            return 0
+        h = 1
+        while h <= n and s[-h] == " ":
+            h += 1
+
+        if h > n:
+            return 0
+        l = h
+        while l <= n and s[-l] != ' ':
+            l += 1
+        return l - h 
