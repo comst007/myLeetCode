08cd58341e73b81b5ff952f814e19bb999823110
Comst <liuzhao88108@gmail.com>
2019-03-08 00:28:00 +0800

    leetcode 70

---
+++ leetcode-70.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n == 3:
+            return 3
+
+        res = [0] * n
+        res[0] = 1
+        res[1] = 2
+        res[2] = 3
+        for i in range(3, n):
+            res[i] = res[i - 1] + res[i - 2]
+
+
+        return res[-1] 
